df4efbbcf870420327601534f6b02813e94c15e3
Domenico Capalbo <capalbodomenico@gmail.com>
2023-05-01 15:39:17 +0300

    header fixed dispatch bug

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,17 +1,14 @@
 import classes from "./header.module.scss";
+import { useDispatch } from "react-redux";
 import NavBar from "../nav/NavBar";
 import { useState } from "react";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { dataSweetActions } from "../../store/data-sweet-slice";
 
 const Header = () => {
   const dispatch = useDispatch();
   const [sticky, setSticky] = useState(false);
 
   useEffect(() => {
-    dispatch(dataSweetActions.resetSweetData());
-
     const isSticky = () => {
       if (window.scrollY >= 107) {
         setSticky(true);
